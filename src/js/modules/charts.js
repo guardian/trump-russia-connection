877a8f314cbb633b4e6bb684f21793627b46ccd1
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-08-01 13:09:37 -0400

    some magic numbers and math to get the graphics looking better

--- src/js/modules/charts.js
+++ src/js/modules/charts.js
@@ -12,12 +12,13 @@ module.exports =  {
         var svg = d3.select(target),
             width = $(target).width(),
             height = $(target).height(),
+            radius = 60,
             data = JSON.parse($(target).attr('data-json'));
 
         var simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function(d) { return d.name; }))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(width / 2, height / 2));
+            .force('link', d3.forceLink().id(function(d) { return d.name; }).strength(1).distance(radius * 2.4).iterations(5))
+            .force('charge', d3.forceManyBody().strength(radius * -30))
+            .force('center', d3.forceCenter(width / 2, height / 2));
 
         var links = [];
 
@@ -33,44 +34,44 @@ module.exports =  {
             }
         }
 
-        var link = svg.append("g")
-            .attr("class", "links")
-            .selectAll("line")
+        var link = svg.append('g')
+            .attr('class', 'links')
+            .selectAll('line')
             .data(links)
-            .enter().append("line")
-            .attr("stroke-width", 1);
+            .enter().append('line')
+            .attr('stroke-width', 5);
 
         // use some html here instead?!??!
 
-        var node = svg.append("g")
-            .attr("class", "nodes")
-            .selectAll("circle")
+        var node = svg.append('g')
+            .attr('class', 'nodes')
+            .selectAll('circle')
             .data(data)
-            .enter().append("circle")
-            .attr("r", 5)
-            .attr("fill", '#ff5b3a')
+            .enter().append('circle')
+            .attr('r', radius)
+            .attr('fill', '#ff5b3a')
 
-        node.append("title")
+        node.append('title')
             .text(function(d) { return d.name; });
 
         simulation
             .nodes(data)
-            .on("tick", ticked);
+            .on('tick', ticked);
 
 
-        simulation.force("link")
+        simulation.force('link')
             .links(links);
 
         function ticked() {
             link
-                .attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
+                .attr('x1', function(d) { return d.source.x; })
+                .attr('y1', function(d) { return d.source.y; })
+                .attr('x2', function(d) { return d.target.x; })
+                .attr('y2', function(d) { return d.target.y; });
 
             node
-                .attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
+                .attr('cx', function(d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)); })
+                .attr('cy', function(d) { return d.y = Math.max(radius, Math.min(width - radius, d.y));; });
           }
 
     }
