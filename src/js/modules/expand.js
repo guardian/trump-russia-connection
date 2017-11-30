d85a6213e14c5ba4b0007d1d803dcb23fdcb88fb
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-11-30 11:08:27 -0500

    Add support for groups

--- src/js/modules/expand.js
+++ src/js/modules/expand.js
@@ -12,6 +12,6 @@ module.exports =  {
     },
 
     expandIndex: function() {
-        $('.mapped-header__questions--index').removeClass('is-collapsed').addClass('is-expanded');
+        $('.mapped-header__index').removeClass('is-collapsed').addClass('is-expanded');
     }
 };
